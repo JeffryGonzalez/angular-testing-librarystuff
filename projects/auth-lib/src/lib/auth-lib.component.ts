2fe6944cbe6df6547cafc7535cc16f666b2d4d94
Jeffry Gonzalez <jeff@hypertheory.com>
2022-05-18 15:28:06 -0400

    before break

--- projects/auth-lib/src/lib/auth-lib.component.ts
+++ projects/auth-lib/src/lib/auth-lib.component.ts
@@ -1,20 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'lib-auth-lib',
   template: `
-    <p>
-      auth-lib works!
-    </p>
+    <h1 data-testid="msg">
+      {{msg}}
+    </h1>
+    <button (click)="party()">Click Me For A Good Time</button>
   `,
   styles: [
   ]
 })
 export class AuthLibComponent implements OnInit {
 
+  @Input() msg = 'auth-lib is go!';
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  party() {
+    this.msg = 'PARTY TIME!';
   }
 
 }
