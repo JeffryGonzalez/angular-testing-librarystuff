8811e413a8e5fe57c5eb31891eb8e36300b6c124
Jeffry Gonzalez <jeff@hypertheory.com>
2022-05-18 16:50:43 -0400

    Update public-api.ts

--- projects/auth-lib/src/public-api.ts
+++ projects/auth-lib/src/public-api.ts
@@ -2,6 +2,14 @@
  * Public API Surface of auth-lib
  */
 
-export * from './lib/auth-lib.service';
-export * from './lib/auth-lib.component';
-export * from './lib/auth-lib.module';
+// export * from './lib/auth-lib.service';
+// export * from './lib/auth-lib.component';
+// export * from './lib/auth-lib.module';
+
+export * from './lib/auth/auth.module';
+export * from './lib/auth/components/login-form/login-form.component';
+export * from './lib/auth/components/user-status/user-status.component';
+export * from './lib/auth/state/index';
+export * from './lib/auth/guards/auth.guard';
+export * from './lib/auth/guards/not-loggedin.guard';
+export * from './lib/auth/state/actions/auth.actions';
